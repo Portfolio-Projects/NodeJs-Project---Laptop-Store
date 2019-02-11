1d13775690fbeede1399af92a00a880365371857
Cosmina Palade <cosmina.palade@gmail.com>
2019-02-11 16:11:10 +0200

    Changing readme file

--- README.md
+++ README.md
@@ -2,15 +2,17 @@
 The Laptop Store is a small Project, which includes a main page completely generated on a Node.js web server. Includes functionalities like URL changes, every time we click on a button and page generation in Node.js each time the users clicks the Back btn. Everything is automatically generated in Node.js.
 
 <p align="center">
-  <img src="">
+  <img src="https://raw.githubusercontent.com/Portfolio-Projects/NodeJs-Project---Laptop-Store/master/img/overview.png">
 </p>
-
+<p align="center">
+  <img src="https://raw.githubusercontent.com/Portfolio-Projects/NodeJs-Project---Laptop-Store/master/img/product.png">
+</p>
 
 ## Technologies used for this Project are:
 - ECMASCRIPT 6
 - CSS3, SASS
 - NODE JS, NPM
-- WEBPACK, BABEL
+- TEMPLATING HTML
 
 ## Folders Structure:
 - data = source code;
@@ -18,14 +20,10 @@ The Laptop Store is a small Project, which includes a main page completely gener
 ## How to Install: 
 - Clone or Download the Project via HTTPS or SSH;
 - Download the npm packages - package.json
-- In package.json file in scripts you can find the npm run commands;
+- Install nodemon via command -> npm install nodemon -g 
+- Set the server to run at the desired address either localhost or server
 - Open the terminal and run the project using the npm command:
 
 ```
         npm run start
 ```
-<p align="center">
-  <img src="">
-</p>
-
-
--- data/index.js
+++ data/index.js
@@ -2,42 +2,26 @@
 // *************************************
 //  IMPORTING PACKAGES from NODE.JS
 // *************************************
-//Example of reading a file system in node js;
-// we give the variable name the same name as the package
 const fs = require('fs');
-// calling http node package and storing into the variable with the same name
 const http = require('http');
 const url = require('url');
-
-
-// *************************************
-//  Using the PACKAGES imported
-// *************************************
-
-// reading the local file
-// here we are using the sync version of file read since it is ok and will run only 1 time when we start our app:
 const json = fs.readFileSync(`${__dirname}/data.json`, `utf-8`);
 const laptopData = JSON.parse(json);
-// console.log(laptopData);
+
 
 
 //creating a server
 const server = http.createServer( (req, res) => {
-  //reading the path name from the req argument: 
-  //using the url module to parse the url into an object 
-  //      and extracting the pathname from the newly created object 
    const pathName = url.parse(req.url, true).pathName;
    let imgPath = `${__dirname}/data/img${pathName}`;
    console.log(imgPath);
 
    const id = url.parse(req.url, true).query.id;
-//    console.log(pathName);
-//    console.log(id);
+
     
 // PRODUCT OVERVIEW
     if( req.url === `/products`|| req.url === `/`) {
         res.writeHead(200, { 'Content-type': 'text/html'});
-        // res.end(`Welcome to PRODUCTS page with id ${id}!`);
 
         fs.readFile(`../templates/overview.html`, 'utf-8', (err, data) => {
 
@@ -52,8 +36,6 @@ const server = http.createServer( (req, res) => {
             });
 
         });
-
-
     }
     // LAPTOP DETAILS
     else if( req.url  === `/laptop?id=${id}`) {
@@ -63,7 +45,6 @@ const server = http.createServer( (req, res) => {
 
             const laptop = laptopData[id];
             const output = replaceTemplate(data, laptop) 
-            // sends the response back to the browser:
             res.end(output);
 
         });
@@ -82,38 +63,13 @@ const server = http.createServer( (req, res) => {
         res.writeHead(404, { 'Content-type': 'text/html'});
         res.end('URL not FOUND!');
     }
-
-
-
 });
 
 server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for requests now');
 })
 
-
-// Classes in ES6 
-// class Car {
-    
-//     // properties attached to your class via constructor;
-//     constructor(brand, model, year) {
-//         this.brand = brand;
-//         this.model= model;
-//         this.year = year;
-//     }
-// // Methods which provides functionality to your class
-// displayCarInfo() {
-//     console.log(`${this.brand} ${this.model} ${this.year}`);
-// }
-
-// }
-
-// const modelBWV = new Car('BMW', 'SPEED', 2015);
-// const modelLambourghini = new Car('LAMBOURGHINI', 'LUX', 2017);
-
-// modelBWV.displayCarInfo();
-// modelLambourghini.displayCarInfo();
-
+// REFACTORING 
 function replaceTemplate(originalHTML, laptop) {
     let output = originalHTML.replace(/{%PRODUCTNAME%}/g, laptop.productName);
     output = output.replace(/{%IMAGE%}/g, laptop.image);
